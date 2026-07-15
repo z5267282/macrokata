95175585233b9957ba0e305f70bc4efeb0f8fcf3
z5267282 <80458779+z5267282@users.noreply.github.com>
2026-07-15 11:32:40 +1000

    exercise 02

--- exercises/02_numbers/main.rs
+++ exercises/02_numbers/main.rs
@@ -4,7 +4,17 @@ fn print_result(num: i32) {
 }
 ////////// DO NOT CHANGE ABOVE HERE /////////
 
-// TODO: create `num!()` macro.
+macro_rules! num {
+    (one) => {
+        1
+    };
+    (two) => {
+        2
+    };
+    (three) => {
+        3
+    };
+}
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
